4f7b5e3339305b8ac99d278bfa0bd0cc8a9d3118
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-06-14 21:05:53 +0200

    Set DBroadcastingActor to replicate

--- Source/AsyncNode/DBroadcastingActor.cpp
+++ Source/AsyncNode/DBroadcastingActor.cpp
@@ -8,6 +8,7 @@ ADBroadcastingActor::ADBroadcastingActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
 	
 	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComp");
 	RootComponent = StaticMeshComp;
